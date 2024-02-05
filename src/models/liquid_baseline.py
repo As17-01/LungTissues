6adad7463b178654ae5b86712dce018527fd5c48
As17-01 <aseliverstov@nes.ru>
2024-02-06 00:29:25 +0300

    Add liquid

--- src/models/liquid_baseline.py
+++ src/models/liquid_baseline.py
@@ -1,27 +1,25 @@
 import torch
 import torch.nn.functional as f
 from ncps.torch import LTC
-from ncps.wirings import AutoNCP
 
 
 class LiquidBaseline(torch.nn.Module):
     # See https://github.com/mlech26l/ncps
     def __init__(self):
         super(LiquidBaseline, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 6, 5)
-        self.conv2 = torch.nn.Conv2d(6, 16, 3)
-        self.rnn = LTC(16 * 62 * 62, AutoNCP(28, 2))
+        self.fc1 = torch.nn.Linear(3 * 256 * 256, 400)
+        self.rnn = LTC(400, 28)
+        self.fc2 = torch.nn.Linear(28, 2)
 
     def forward(self, x):
-        x = f.max_pool2d(f.relu(self.conv1(x)), (2, 2))
-        x = f.max_pool2d(f.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x, _ = self.rnn(x)
-        return x
+        num_batches = x.shape[0]
+        num_slices = x.shape[1]
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+        hidden = torch.zeros(num_batches, 28)
+
+        x = x.view(num_batches, num_slices, -1)
+        x = self.fc1(x)
+        x, hidden = self.rnn(x, hidden)
+        x = self.fc2(x[:,-1,:])
+
+        return x
