b5eb4ecfe6c23c8778e74e3c01e9f8d130ff92d0
As17-01 <aseliverstov@nes.ru>
2024-02-08 01:46:53 +0300

    Update init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
 from src.models.cnn_baseline import CNNBaseline
 from src.models.liquid_baseline import LiquidBaseline
 from src.models.lstm_baseline import LSTMBaseline
+from src.models.inception3 import ModifiedInception3
+from src.models.resnet18 import ModifiedResNet18
