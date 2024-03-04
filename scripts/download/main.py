bc9b249a6f9a3202eb8d20c4f5232f5678f11927
As17-01 <aseliverstov@nes.ru>
2024-03-04 16:40:52 +0300

    Fix json download

--- scripts/download/main.py
+++ scripts/download/main.py
@@ -16,7 +16,7 @@ class YandexDiskDownloader:
         self.download_location = download_location
         self.batch = batch
 
-    def _download_file(self, path, save_dir):
+    def _download_file(self, path, save_dir, unpack=True):
         url = f"https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={self.link}&path={path}"
         response = requests.get(url)
         download_url = response.json()["href"]
@@ -31,10 +31,11 @@ class YandexDiskDownloader:
                     file.flush()
             logger.info(f"Saved to {save_path_zip}")
 
-        save_path = save_dir / file_name.split(".")[0]
-        with zipfile.ZipFile(save_path_zip, mode="r") as archieve:
-            archieve.extractall(save_path)
-        logger.info(f"Zip is unpacked to {save_path}")
+        if unpack:
+            save_path = save_dir / file_name.split(".")[0]
+            with zipfile.ZipFile(save_path_zip, mode="r") as archieve:
+                archieve.extractall(save_path)
+            logger.info(f"Zip is unpacked to {save_path}")
 
         os.remove(save_path_zip)
 
@@ -48,12 +49,12 @@ class YandexDiskDownloader:
         logger.info(f"The chosen batch is {self.batch}")
         for i, file in enumerate(all_images):
             if  self.batch[0] <= i and i < self.batch[1]:
-                logger.info(f"File {i + 1}/{len(all_images)}: {file['path']}")
+                logger.info(f"File {i + 1}/{len(all_images)}: {file['name']}")
                 self._download_file(path=file['path'], save_dir=self.download_location / "images")
 
         logger.info(f"Loading metadata")
-        self._download_file(path="/biospecimen.cart.2024-01-18.json", save_dir=self.download_location)
-        self._download_file(path="metadata.cart.2024-01-18.json", save_dir=self.download_location)
+        self._download_file(path="/biospecimen.cart.2024-01-18.json", save_dir=self.download_location, unpack=False)
+        self._download_file(path="/metadata.cart.2024-01-18.json", save_dir=self.download_location, unpack=False)
 
         logger.info("Download complete.")
 
