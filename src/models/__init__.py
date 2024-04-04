7aadb70ee3619967d55feb95283cac3c51c87036
As17-01 <aseliverstov@nes.ru>
2024-04-04 17:55:16 +0300

    Add lstm

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from src.models.cnn_baseline import CNNBaseline
 from src.models.mnist_cnn_baseline import MNISTCNNBaseline
 from src.models.mnist_liquid_baseline import MNISTLiquidBaseline
+from src.models.mnist_lstm_baseline import MNISTLSTMBaseline
---
+++ src/models/mnist_lstm_baseline.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn.functional as f
+from ncps.torch import LTC
+
+from src.models.base import BaseModel
+from src.utils import get_default_device
+
+
+class MNISTLSTMBaseline(BaseModel):
+    def __init__(self):
+        super(MNISTLSTMBaseline, self).__init__()
+        self.lstm = torch.nn.LSTM(28 * 28, 40, 2, batch_first=True)
+        self.fc1 = torch.nn.Linear(40, 1)
+
+    def forward(self, x):
+        hidden = (
+            torch.autograd.Variable(torch.zeros(2, 40, device=get_default_device())),
+            torch.autograd.Variable(torch.zeros(2, 40, device=get_default_device())),
+        )
+        cur_x = x.view(x.shape[0], -1)
+
+        cur_x, hidden = self.lstm(cur_x, hidden)
+        cur_x = f.sigmoid(self.fc1(cur_x))
+
+        return cur_x
