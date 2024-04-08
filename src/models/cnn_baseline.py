256cc7a7809a1eec09c023c5720a055139c65615
As17-01 <aseliverstov@nes.ru>
2024-04-08 18:37:15 +0300

    change architecture

--- src/models/cnn_baseline.py
+++ src/models/cnn_baseline.py
@@ -7,19 +7,24 @@ from src.models.base import BaseModel
 class CNNBaseline(BaseModel):
     def __init__(self):
         super(CNNBaseline, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 6, 5)
-        self.conv2 = torch.nn.Conv2d(6, 16, 3)
-        self.conv3 = torch.nn.Conv2d(16, 18, 3)
-        self.fc1 = torch.nn.Linear(18 * 35 * 35, 120)
-        self.fc2 = torch.nn.Linear(120, 84)
-        self.fc3 = torch.nn.Linear(84, 1)
+        self.conv1 = torch.nn.Conv2d(3, 32, 3, 1, 1)
+        self.conv2 = torch.nn.Conv2d(32, 64, 3, 1, 1)
+        self.conv3 = torch.nn.Conv2d(64, 128, 3, 1, 1)
+        self.conv4 = torch.nn.Conv2d(128, 256, 3, 1, 1)
+        self.conv5 = torch.nn.Conv2d(256, 256, 3, 1, 1)
+        self.conv6 = torch.nn.Conv2d(256, 256, 3, 1, 1)
+        self.fc1 = torch.nn.Linear(256 * 4 * 4, 1000)
+        self.fc2 = torch.nn.Linear(1000, 1)
 
     def forward(self, x):
         cur_x = f.max_pool2d(f.relu(self.conv1(x)), (2, 2))
         cur_x = f.max_pool2d(f.relu(self.conv2(cur_x)), (2, 2))
         cur_x = f.max_pool2d(f.relu(self.conv3(cur_x)), (2, 2))
+        cur_x = f.max_pool2d(f.relu(self.conv4(cur_x)), (2, 2))
+        cur_x = f.max_pool2d(f.relu(self.conv5(cur_x)), (2, 2))
+        cur_x = f.max_pool2d(f.relu(self.conv6(cur_x)), (2, 2))
+
         cur_x = cur_x.view(x.shape[0], -1)
         cur_x = f.relu(self.fc1(cur_x))
-        cur_x = f.relu(self.fc2(cur_x))
-        cur_x = f.sigmoid(self.fc3(cur_x))
+        cur_x = f.sigmoid(self.fc2(cur_x))
         return cur_x
