e375dde70cc6efd1f8a3dcc00117564e651eea42
As17-01 <aseliverstov@nes.ru>
2024-04-17 20:33:59 +0300

    Add early stopper

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,7 @@ import json
 import pathlib
 
 import torch
-
-# from loguru import logger
+from loguru import logger
 
 
 def get_default_device():
@@ -48,6 +47,24 @@ def evaluate(model, val_loader, expand: bool):
     return model.validation_epoch_end(outputs)
 
 
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float("inf")
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+
 def fit(epochs, lr, model, train_loader, val_loader, expand: bool, opt_func=torch.optim.Adam):
     """Train the model using gradient descent."""
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -56,6 +73,7 @@ def fit(epochs, lr, model, train_loader, val_loader, expand: bool, opt_func=torc
 
     history = []
     optimizer = opt_func(model.parameters(), lr)
+    early_stopper = EarlyStopper(patience=5, min_delta=0.001)
     for epoch in range(epochs):
         # logger.info("Training Phase...")
         model.train()
@@ -79,4 +97,8 @@ def fit(epochs, lr, model, train_loader, val_loader, expand: bool, opt_func=torc
         torch.save(model.state_dict(), save_dir / f"epoch{epoch}.pt")
         with open(save_dir / f"epoch{epoch}.json", "w") as file:
             json.dump(history[-1], file)
+
+        if early_stopper.early_stop(result["val_loss"]):
+            logger.info(f"Early stop at epoch {epoch}!")
+            break
     return history
