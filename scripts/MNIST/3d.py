2455537e895c3bf4f16be2e3dda9a397a361a110
As17-01 <aseliverstov@nes.ru>
2024-05-13 01:11:53 +0300

    Add sbatch

--- scripts/MNIST/3d.py
+++ scripts/MNIST/3d.py
@@ -18,43 +18,10 @@ import src.utils
 
 @hydra.main(config_path="configs", config_name="config", version_base="1.2")
 def main(cfg: DictConfig) -> None:
-    device = src.utils.get_default_device()
-    logger.info(f"Current device is {device}")
-
-    num_workers = cfg.training_params.num_workers
-    batch_size = cfg.training_params.batch_size
-    lr = cfg.training_params.learning_rate
-    num_epochs = cfg.training_params.num_epochs
-
-    cfg_dct = omegaconf.OmegaConf.to_container(cfg, resolve=True)
-    registry = Registry()
-    registry.add_from_module(src.datasets, prefix="src.datasets.")
-    registry.add_from_module(src.models, prefix="src.models.")
 
     train_data = medmnist.NoduleMNIST3D(split="train", download=True)
     valid_data = medmnist.NoduleMNIST3D(split="val", download=True)
     test_data = medmnist.NoduleMNIST3D(split="test", download=True)
-
-    train_dataloader = DataLoader(train_data, num_workers=num_workers, batch_size=batch_size, shuffle=True)
-    valid_dataloader = DataLoader(valid_data, num_workers=num_workers, batch_size=batch_size, shuffle=False)
-    test_dataloader = DataLoader(test_data, num_workers=num_workers, batch_size=batch_size, shuffle=False)
-
-    train_dataloader = src.utils.DeviceDataLoader(train_dataloader, device)
-    valid_dataloader = src.utils.DeviceDataLoader(valid_dataloader, device)
-    test_dataloader = src.utils.DeviceDataLoader(test_dataloader, device)
-
-    model = registry.get_from_params(**cfg_dct["model"])
-    src.utils.to_device(model, device)
-
-    with torch.no_grad():
-        model.eval()
-        history = [src.utils.evaluate(model, valid_dataloader, time_dimension=1)]
-
-    history += src.utils.fit(num_epochs, lr, model, train_dataloader, valid_dataloader, time_dimension=1)
-
-    with torch.no_grad():
-        model.eval()
-        model.epoch_end(num_epochs, src.utils.evaluate(model, test_dataloader, time_dimension=1), "test")
 
 
 if __name__ == "__main__":
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -11,4 +11,5 @@ from src.models.mnist2d.mnist_liquid_baseline import MNIST2dLiquidBaseline
 from src.models.mnist2d.mnist_liquid_baseline_exp0 import MNIST2dLiquidBaselineExp0
 from src.models.mnist2d.mnist_lstm_baseline import MNIST2dLSTMBaseline
 from src.models.mnist3d.mnist_cnn_baseline import MNIST3dCNNBaseline
+from src.models.mnist3d.mnist_full_liquid_baseline import MNIST3dFullLiquidBaseline
 from src.models.mnist3d.mnist_liquid_baseline import MNIST3dLiquidBaseline
