bcff58a2c28b8cd28304da7b498835b79270b41e
As17-01 <aseliverstov@nes.ru>
2024-05-17 02:45:34 +0300

    Increase learning rate

--- src/models/mnist3d/mnist_full_liquid_conf.py
+++ src/models/mnist3d/mnist_full_liquid_conf.py
@@ -121,4 +121,25 @@ class MNIST3dFullLiquidConf4(BaseModel):
         output = f.relu(self.rnn1(output)[0])
         output = self.rnn2(output)[0]
         return f.sigmoid(torch.mean(output, 1))
+    
+class MNIST3dFullLiquidConf5(BaseModel):
+    def __init__(self):
+        super(MNIST3dFullLiquidConf5, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 6, 5)
+        self.conv2 = torch.nn.Conv2d(6, 16, 3)
+        self.rnn1 = LTC(16 * 5 * 5, AutoNCP(30, 1))
+
+    def forward(self, x):
+        batch_size, time_steps, C, H, W = x.size()
+        output = torch.tensor([], device=get_default_device())
+        for i in range(time_steps):
+            cur_x = f.max_pool2d(f.relu(self.conv1(x[:, i, :, :, :])), (2, 2))
+            cur_x = f.max_pool2d(f.relu(self.conv2(cur_x)), (2, 2))
+            cur_x = cur_x.view(batch_size, -1)
+
+            cur_x = cur_x.unsqueeze(1)
+            output = torch.cat((output, cur_x), 1)
+
+        output = self.rnn1(output)[0]
+        return f.sigmoid(torch.mean(output, 1))
 
