be093f45bd66707121ef57949abbfa444bd89883
As17-01 <aseliverstov@nes.ru>
2024-05-17 14:57:23 +0300

    Run small cnn

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -10,7 +10,7 @@ from src.models.mnist2d.mnist_linear_baseline import MNIST2dLinearBaseline
 from src.models.mnist2d.mnist_liquid_baseline import MNIST2dLiquidBaseline
 from src.models.mnist2d.mnist_liquid_baseline_exp0 import MNIST2dLiquidBaselineExp0
 from src.models.mnist2d.mnist_lstm_baseline import MNIST2dLSTMBaseline
-from src.models.mnist3d.mnist_cnn_baseline import MNIST3dCNNBaseline
+from src.models.mnist3d.mnist_cnn_baseline import MNIST3dCNNBaseline, MNIST3dCNNBaselineSmall
 from src.models.mnist3d.mnist_full_liquid_baseline import MNIST3dFullLiquidBaseline
 from src.models.mnist3d.mnist_liquid_baseline import MNIST3dLiquidBaseline
 from src.models.mnist3d.mnist_full_liquid_conf import MNIST3dFullLiquidConf0, MNIST3dFullLiquidConf1, MNIST3dFullLiquidConf2, MNIST3dFullLiquidConf3, MNIST3dFullLiquidConf4, MNIST3dFullLiquidConf5
